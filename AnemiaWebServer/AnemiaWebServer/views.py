91f425f894aee149f7c37da0c1a4790a3dc5aa8d
SamedovResul <samedovrasul7@gmail.com>
2024-05-02 17:24:04 +0400

    .

--- AnemiaWebServer/AnemiaWebServer/views.py
+++ AnemiaWebServer/AnemiaWebServer/views.py
@@ -73,9 +73,13 @@ class PostView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
     def get(self, request, *args, **kwargs):
-        posts = Post.objects.all()
-        serializer = PostSerializer(data=request.data)
-        return Response(serializer, status=status.HTTP_201_CREATED)
+       posts_serializer = PostSerializer(data=request.data)
+       if posts_serializer.is_valid():
+            # 
+        posts_serializer.save()
+        print(posts_serializer.data)
+        AiRes = Check_image(posts_serializer.data)
+        return Response(AiRes, status=status.HTTP_201_CREATED)
 
     def post(self, request, *args, **kwargs):
         posts_serializer = PostSerializer(data=request.data)
