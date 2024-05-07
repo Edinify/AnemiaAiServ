a37e9d73cd6343340d1ba39778fc276941d8f817
SamedovResul <samedovrasul7@gmail.com>
2024-05-07 15:36:28 +0400

    add origin

--- AnemiaWebServer/AnemiaWebServer/settings.py
+++ AnemiaWebServer/AnemiaWebServer/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['anemiaapp.azurewebsites.net']
 
 INSTALLED_APPS = [
     'rest_framework',
-    # 'django_cassandra_engine',
+    'corsheaders',
     'AnemiaWebServer',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -47,12 +47,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'AnemiaWebServer.urls'
 
